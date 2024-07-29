158ec8101bf3ac882aacb918513cd42d88334827
BananBogdan <grigorijvidakin903@gmail.com>
2024-07-29 11:29:26 +0300

    class Box

--- main_file.py
+++ main_file.py
@@ -17,13 +17,16 @@ class ProgectLavel:
         self.screen_rect = self.screen.get_rect()
         pygame.mouse.set_visible(False)
 
-        self.x = self.width 
-        self.y = self.height 
+
+        self.x = self.width / 2 - ( self.box_width / 2) 
+        self.y = self.height / 2 - ( self.box_height / 2 )
 
         self.box_up = False
         self.box_down = False
         self.box_right = False
         self.box_left = False
+
+
 
 
     def run_game(self):
@@ -84,8 +87,39 @@ class ProgectLavel:
             self.screen, (255, 0, 0), (self.x, self.y, self.box_width, self.box_height)
         )
 
-
 if __name__ == "__main__":
     # Создание экземпляра и запуск игры
     pg_game = ProgectLavel()
     pg_game.run_game()
+
+
+
+
+class Box:
+    def __init__(self):
+        self.width = 800
+        self.height = 600
+        self.speed = 20
+        self.box_width = 100
+        self.box_height = 100
+
+    def run_game(self):
+        """ОСНОВНОЙ ЦИКЛ"""
+        while True:
+            self._move_box()
+            
+    def _move_box(self):
+            if self.box_up == True and self.y > 0:
+                self.y -= self.speed
+            if self.box_down == True and self.y < 500:
+                self.y += self.speed
+            if self.box_right == True and self.x < 700:
+                self.x += self.speed
+            if self.box_left == True and self.x > 0:
+                self.x -= self.speed
+
+
+    def rectCeate(self):
+        self.rect = pygame.draw.rect(
+            self.screen, (255, 0, 0), (self.x, self.y, self.box_width, self.box_height)
+        )
